e1c1764a436100188784f2626e29b185fa46e3cd
dalimark-nemsu <dalimarktenio@gmail.com>
2022-09-22 10:34:03 +0800

    Setup login register system

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -79,5 +79,12 @@
             @yield('content')
         </main>
     </div>
+
+
+         <!-- Scripts -->
+         <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}" defer></script>
+
+ 
+
 </body>
 </html>
